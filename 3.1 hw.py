5fa1f1277b11490338f63649ba8fd5f4d1a32aa5
Mohit819 <immohitrana06@gmail.com>
2025-01-02 17:37:29 -0500

    Add files via upload

---
+++ 3.1 hw.py
@@ -0,0 +1,18 @@
+##address="29 agricola road"
+##address2 = address[3::]
+##address3 = address[:2:]
+##print(address2)
+##print(address3)
+
+##e = "The Pirates of the Caribbean"
+##print(e[2:3:])
+##print(e[9:10:])
+##print(e[17:18:])
+##print(e[25:26:])
+##print(e[-3:-4:-1])
+##print(e[-11:-12:-1])
+##print(e[-19:-20:-1])
+##print(e[-26:-27:-1])
+##
+
+print("mmmwqawteerrt ybuoitotplaesmm"[3:27:2])
