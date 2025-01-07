6424fc6bd4a21f58dadef390ef480f1a70f02029
Mohit819 <immohitrana06@gmail.com>
2025-01-07 17:12:47 -0500

    Add files via upload

---
+++ 3.2 cw.py
@@ -0,0 +1,39 @@
+##code = "eg!srtohrsmecvpbixndexg avgrentvioxrsstiesmy mzakh \
+##leeukrja'vyuaoovdYc"
+##print(code[-2::-3])
+
+##a = "boop".count("o") # a is 2 because there are 2 o's in boop
+##b = "title".find("t") # b is 0 because the first t is at index 0
+##c = "StR1nG".isalnum()
+### c is True because the string is only letters and numbers
+##d = "Hello World!".isalpha()
+### d is False because there is a space and !
+##e = "1234".isdigit()
+### e is True because our string is all numbers
+##f = "HAYDEN".isupper()
+### f is True because all the letters are uppercase
+##g = "hayden123".islower()
+### g is True because all the letters are lowercase
+##h = "banana".replace('a', 'u')
+### h is "bununu" because all a's are replaced with u's
+##j = " jo jo".title() # j is "Jo Jo"
+
+##a method is like a function for indices and strings
+
+##i = input("whats ur name")
+##print(i.title())
+
+##num = input("What is your favourite number?")
+##while num.isdigit() == False:
+##    if num.isdigit() == False:
+##        print("write ur number with an actual number")
+##    num = input("What is your favourite number?")
+
+##i = input("number pls")
+##while i.isdigit()==False :
+##    print("invalid input, enter only numbers")
+##    i = input("number pls")
+
+var = "Mow mow mow your boat".replace("M","R")
+var2=var.replace("m","r")
+print(var2)
