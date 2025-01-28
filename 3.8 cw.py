7db58903e36c82561b52e138b11b544d899d23b3
Mohit819 <immohitrana06@gmail.com>
2025-01-27 20:02:51 -0500

    Add files via upload

---
+++ 3.8 cw.py
@@ -0,0 +1,24 @@
+##menu = {"oatmeal": 3, "avocado toast": 6, "carrot juice": 5, "blueberry muffin": 2}
+##print(menu)
+
+menu = {"milk":4,"eggs":3,"bread":2,"cake":5}
+
+my_empty_dictionary={}
+
+animals_in_zoo = {}
+animals_in_zoo.update({"zebras":8,"monkeys":12,"dinosaurs":0})
+print(animals_in_zoo)
+
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+user_ids.update({"theLooper":138475,"stringQueen":85739})
+print(user_ids)
+
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Aeck", "Best Actress": "Emma Stone", "Animated Feature":"Zootopia"}
+oscar_winners["Supporting Actress"] = "Viola Davis"
+oscar_winners["Best Picture"] = "Moonlight"
+print(oscar_winners)
+
+students = {'Jenny': 61, 'Alexus': 70, 'Sam': 67, 'Grace': 64}
+students["James"] = 77
+students["Sam"] = 66
+print(students)
