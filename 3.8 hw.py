1b5f69b6dc1a04e75a5066291600764cbc266277
Mohit819 <immohitrana06@gmail.com>
2025-01-30 17:34:44 -0500

    Add files via upload

---
+++ 3.8 hw.py
@@ -0,0 +1,24 @@
+dictionary = {"coffee":2,"bagel":3,"donut":1,"sandwich":5}
+
+empty_dict={}
+
+sports_play = {}
+sports_play["soccer"] = 7
+sports_play["rugby"]=9
+sports_play["basketball"]=11
+print(sports_play)
+
+user_ids = {"brianDog": 9018293, "jennyCat": 119238}
+user_ids.update({"cuteDuck":178375,"sweetPie":92537})
+print(user_ids)
+
+movies= {"Best Picture": "Mickey Mouse", "Best Actor": "Mickey", "Best Actress": "Minnie", "Animated Feature": "Mouse"}
+movies["Supporting Actree"]="Anyone"
+movies["Best Picture"]=["Frozen"]
+print(movies)
+
+
+fruits = {'apple': 6, 'strawberry': 7, 'watermelon': 3, 'pineapple': 4}
+fruits["orange"]=17
+fruits["watermelon"]=5
+print(fruits)
