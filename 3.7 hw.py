e4bd5ba402a1bc38f9171b9f473db028f80accb3
Mohit819 <immohitrana06@gmail.com>
2025-01-30 19:27:05 -0500

    Add files via upload

---
+++ 3.7 hw.py
@@ -0,0 +1,32 @@
+##def sum2(*nums):
+##    result = 0
+##    for i in nums:
+##        result += i
+##    return result
+##print(sum2(1,2,3,4,5))
+
+##def len2(length):
+##    length2 = 0
+##    for i in length:
+##        length2 +=1
+##    return length2
+##print(len2("Hello "))
+
+##def range2(end,start=0,skip=1):
+##    lst = []
+##    for i in range(start,end,skip):
+##        lst.append(i) 
+##    return lst
+##print(range2(start=3, end=20, skip=3))
+
+def slice(lst,start,skip,end=-1):
+    sli =[]
+    if end == -1:
+        end = len(lst)
+    for i in range(start,end,skip):
+        sli.append(lst[i])
+    return sli
+
+lst2=["hi","hello","hey"]
+w= slice(lst2,1,1,2)
+print(w)
