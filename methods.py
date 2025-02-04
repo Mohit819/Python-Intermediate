6d215b9727d756f531f8727d292802784ae1faa5
Mohit819 <immohitrana06@gmail.com>
2025-02-03 19:53:06 -0500

    Add files via upload

---
+++ methods.py
@@ -0,0 +1,15 @@
+a = "boop".count("o") # a is 2 because there are 2 o's in boop
+b = "title".find("t") # b is 0 because the first t is at index 0
+c = "StR1nG".isalnum()
+# c is True because the string is only letters and numbers
+d = "Hello World!".isalpha()
+# d is False because there is a space and !
+e = "1234".isdigit()
+# e is True because our string is all numbers
+f = "HAYDEN".isupper()
+# f is True because all the letters are uppercase
+g = "hayden123".islower()
+# g is True because all the letters are lowercase
+h = "banana".replace('a', 'u')
+# h is "bununu" because all a's are replaced with u's
+j = " jo jo".title() # j is "Jo Jo"
